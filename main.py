879d4e541a1f048be737a0f6184e35a3c1e5d28a
Sayam Pradhan <112542130+sayampradhan@users.noreply.github.com>
2023-01-21 18:58:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # import neccessary libraries
 import qrcode as qr
+from PIL import Image
 
 # main qr content input from the user
 data = input("Enter some data: ")
@@ -10,28 +11,29 @@ y_colour = input("Do you want to add colour? (y/n)\n")
 if y_colour.lower() == 'y':
     colour = input("Write the colour: ")
 
-try:
-    # features of QR
-    features = qr.QRCode(version=1, box_size=20, border=4)
-    features.add_data(f'{data}')
-    features.make(fit=True)
+else:
+    colour = 'black'
 
-except ValueError:
-    print("Invalid input....")
-
-except:
-    print("Ummmm... Some Error Occurred...")
+# features of qr
+features = qr.QRCode(version=1, box_size=50, border=4)
+features.add_data(f'{data}')
+features.make(fit=True)
 
 # qr image generation
-    generate_image = features.make_image(
-        fill_color=f"{colour}", back_color="white")
+generate_image = features.make_image(
+    fill_color=f"{colour}", back_color="white")
 
 # name of the file
 name = input("Write the name of the qr image you want to save: ")
 
+# generation of image
 try:
     generate_image.save(f'{name}.png')
     print("successfully created....")
 
 except:
-    print("Ummmm.... Some error occurred while generating QR...!!")
+    print("Ummmm.... Something gone wrong...!!")
+
+# opening the qr file
+qr_image = Image.open(f'{name}.png', 'r')
+qr_image.show()
